f329ffb44c04355aa384b0a2d3614913d5534ee5
thomasphillips1 <63131802+thomasphillips1@users.noreply.github.com>
2020-12-23 11:20:39 -0600

    Update ohinstall.ps1

--- ohinstall.ps1
+++ ohinstall.ps1
@@ -2,4 +2,11 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Install Software
-choco install microsoft-teams microsoft-edge zoom obs-studio obs-ndi webex-meetings gotomeeting bluejeansapp -y
+choco install microsoft-edge -y
+choco install zoom -y
+choco install obs-studio -y
+choco install obs-ndi -y
+choco install webex-meetings -y
+choco install gotomeeting -y
+choco install bluejeansapp -y
+choco install microsoft-teams -y
